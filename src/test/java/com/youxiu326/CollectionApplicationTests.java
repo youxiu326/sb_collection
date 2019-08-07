26d22aa746d8e93276a032a417158761b676d8da
youxiu3266 <2473538988@qq.com>
2019-08-07 09:12:35 +0800

    提交代码

--- src/test/java/com/youxiu326/CollectionApplicationTests.java
+++ src/test/java/com/youxiu326/CollectionApplicationTests.java
@@ -5,12 +5,21 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CollectionApplicationTests {
 
     @Test
     public void contextLoads() {
+
+        List list = new ArrayList();
+
+        LinkedList linkedList = new LinkedList();
+
     }
 
 }
